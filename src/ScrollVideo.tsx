1fae32a8687bbe9a148b42257b4afe6dfa380375
Naeem Hussain <hani.brahim130@gmail.com>
2026-08-03 13:35:46 +0000

    Fix invalid newline escapes in scroll video component

--- src/ScrollVideo.tsx
+++ src/ScrollVideo.tsx
@@ -254,7 +254,9 @@ export default function ScrollVideo({ scrollRef }: ScrollVideoProps) {
     return () => window.removeEventListener('resize', handleResize)
   }, [])
   
-  return (\n    <div className="fixed inset-0 z-0 overflow-hidden bg-page pointer-events-none">\n      {/* Poster */}
+  return (
+    <div className="fixed inset-0 z-0 overflow-hidden bg-page pointer-events-none">
+      {/* Poster */}
       <img
         ref={posterRef}
         src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260729_102822_0e6c87e8-c141-4744-bf32-ad30db296371.mp4"
